05d3b9661f8426a3afa79af18b4ed289a5765db8
bitst0rm <bitst0rm@users.noreply.github.com>
2023-11-06 16:34:52 +0100

    Fix to build with MSYS2 ref: https://github.com/msys2/MINGW-packages/blob/master/mingw-w64-winpty/winpty-0.4.3-mingw-fix.patch

--- src/subdir.mk
+++ src/subdir.mk
@@ -2,4 +2,4 @@ include src/agent/subdir.mk
 include src/debugserver/subdir.mk
 include src/libwinpty/subdir.mk
 include src/tests/subdir.mk
-include src/unix-adapter/subdir.mk
+#include src/unix-adapter/subdir.mk
